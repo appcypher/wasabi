36ddb07472a4398ab27ff82d1c87d9ccb751fb08
Steve Akinyemi <appcypher@outlook.com>
2019-02-14 00:01:28 +0100

    Add waconfigure and wamake

---
+++ src/bin/waconfigure.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("\n=== [ waconfigure ] ===\n");
+    println!("\n=== [ waconfigure ] ===\n");
+}
---
+++ src/bin/wamake.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("\n=== [ wamake ] ===\n");
+    println!("\n=== [ wamake ] ===\n");
+}
